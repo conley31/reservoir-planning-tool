f1976631d56233d0cc6d6ee6f979374c55a30ddf
marsha88 <marsha88@purdue.edu>
2017-02-14 20:28:21 -0500

    updated TDPAlg.js

--- TDPAlg.js
+++ TDPAlg.js
@@ -12,15 +12,13 @@ for(i = 0; i < numberOfIncrements; i++){
 	var pondVolume = pondVolSmallest + (i * pondVolIncrement);
 	var pondArea = pondVolume/pondDepth;						//TODO: specify on front-end that the pondDepth can't be zero.
 
-	/*
-	while( !exists(time)){
-	//The following should be done for every year, month, day
+
+
+	//The following should be done for every year, month, day and requires inputs from the database.
 	var precipDay = //from database;
 	var pondPrecipVolDay = percipDay * pondArea;
 	
 	
-	}	
-	*/
 }
 
 
