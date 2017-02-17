f58cd3cfd72c8047fb37f684b9360f2e21fdafc6
Jonah Heeren <jonah.heeren@me.com>
2017-02-17 12:50:10 -0500

    A great way to smash memory

--- db/setup.js
+++ db/setup.js
@@ -3,30 +3,6 @@ var CSV = require('fast-csv');
 var nestedCSV = require('fast-csv');
 var fs = require('fs');
 var nested_fs = require('fs');
-var async = require('async');
-
-var stream = fs.createReadStream("index.csv");
-
-async.waterfall([
-  function(callback) {
-    CSV
-    .fromStream(stream, {headers : ["FID", "ID", "Latitude", "Longitude", "TextFile", "URL"]})
-      .on("data", function(data){
-        console.log(data["ID"] + ":" +  data["TextFile"]);
-        callback(null, "daily_files/" + data["TextFile"]);
-      });
-  },
-  function(data, callback) {
-    var nestedStream = nested_fs.createReadStream("daily_files/" + data["TextFile"]);
-    nestedCSV
-        .fromStream(nestedStream, {headers : ["year", "month", "day", "drainflow", "precipitation", "PET"]})
-          .on("data", function(nested_data){
-            console.log(nested_data["year"] + ":" +  nested_data["month"] + ":" + nested_data["day"] + ":" + nested_data["PET"]);
-          })
-          .on("end", function(){
-            console.log("done");
-      });
-  }])
 
 
 var connection = mysql.createConnection({
@@ -40,3 +16,51 @@ connection.connect(function(err) {
   if (err) throw err
   console.log('You are now connected...')
 })
+
+var createTable = "CREATE TABLE IF NOT EXISTS " 
+var values = `(ID INT NOT NULL AUTO_INCREMENT,
+  RecordDate DATE DEFAULT NULL,
+  Drainflow FLOAT DEFAULT NULL,
+  Precipitation FLOAT DEFAULT NULL,
+  PET FLOAT DEFAULT NULL,
+  PRIMARY KEY (ID)
+)`
+
+var stream = fs.createReadStream("index.csv");
+CSV
+	.fromStream(stream, {headers : ["FID", "ID", "Latitude", "Longitude", "TextFile", "URL"]})
+    .on("data", function(data){
+      connection.query(createTable + "Location" + data["ID"] + " " + values);
+      var nestedStream = nested_fs.createReadStream("daily_files/" + data["TextFile"]);
+      nestedCSV
+        .fromStream(nestedStream, {headers : ["year", "month", "day", "drainflow", "precipitation", "PET"]})
+          .on("data", function(nested_data){
+            var insertValues = { RecordDate: nested_data["year"] + "-" + nested_data["month"] + "-" + nested_data["day"],
+                                Drainflow: nested_data["drainflow"],
+                                Precipitation: nested_data["precipitation"],
+                                PET: nested_data["PET"]};
+            var query = connection.query('INSERT INTO Location' +  data["ID"] + ' SET ?', insertValues, function(err, result) {
+              if(error) {
+                return connection.rollback(function() {
+                  throw error;
+                });
+              }
+              connection.commit(function(err) {
+                if(err) {
+                  return connection.rollback(function() {
+                    throw err;
+                  })
+                }
+              })
+            })
+          })
+          .on("end", function(){
+            console.log("done");
+          });
+    })
+    .on("end", function(){
+         console.log("done");
+    });
+
+
+
