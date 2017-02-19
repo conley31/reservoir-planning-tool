0a9b41e5261605056858e580adec3dc7f984b380
marsha88 <marsha88@purdue.edu>
2017-02-19 12:59:47 -0500

    The body of the algorithm is complete. I still need to meet with proj owners to figure out the specs.

--- TDPAlg.js
+++ TDPAlg.js
@@ -2,7 +2,7 @@
    -------TDPAlg.js**-------
 Notes: This file exports the algorithm developed by the Transforming Drainage Project.
 
- */
+*/
 
 module.exports = function(pondVolSmallest, pondVolLargest, pondVolIncrement, pondDepth, maxSoilMoisture, irrigationArea, irrigationDepth){		//TODO: add last argument.
 	const seepageVolDay = 0.01; //ft
@@ -15,23 +15,22 @@ module.exports = function(pondVolSmallest, pondVolLargest, pondVolIncrement, pon
 
 		var soilMoistureDepthDayPrev = maxSoilMoisture;
 		var waterVolDayPrev = //TODO: what should the default here be?
-		//The following should be done for every year, month, day and requires inputs from the database.
 
+		//The following should be done for every year, month, day and requires inputs from the database.
 		var precipDepthDay = //from database;
 		var evapDepthDay = //from database;
 		var irrigationVolDay = //from db;
 		var waterVolDay = //from db;
 		var pondPrecipVolDay = percipDay * pondArea;
-		var soilMoistureDepthDay = soilMoistureDepthDayPrev + precipDepthDay – evapDepthDay;	//TODO: still waiting on frankenb to tell me if this is the correct statement.
+		var soilMoistureDepthDay = soilMoistureDepthDayPrev + precipDepthDay – evapDepthDay;
 		var deficitVolDay;
 		var inflowVolDay = //from db;
 
 		if(soilMoistureDepthDay < 0.5*maxSoilMoisture){
 			irrigationVolDay = irrigationDepth * irrigationArea;
-
 			//CHECK WITH FRANKENB TO MAKE SURE THIS IF STATEMENT SHOULD BE NESTED.
 			if(irrigationVolDay > waterVolDay){
-				deficitVolDay = (IrrigationVolDay – WaterVolDay)/PondArea;
+				deficitVolDay = (irrigationVolDay – waterVolDay)/PondArea;
 			}
 			else{
 				deficitVolDay = 0;
@@ -43,6 +42,7 @@ module.exports = function(pondVolSmallest, pondVolLargest, pondVolIncrement, pon
 		Read EvaporationVolDay //read from regional input file?????
 		//what is the difference between evapVolDay and evapDepthDay????
 		var waterVolDay = waterVolDayPrev + inflowVolDay + pondPrecipVolDay - irrigationVolDay - seepageVolDay – evapVolDay;
+
 		var bypassFlow, bypassFlowVolDay; 	//what is the difference here or are they the same?
 
 		if(WaterVolDay > PondVol){
@@ -55,12 +55,28 @@ module.exports = function(pondVolSmallest, pondVolLargest, pondVolIncrement, pon
 
 		pondWaterDepthDay = waterVolDay/pondArea;
 
-		//update soilMoistureDepthDayPrev before loop ends
+
+		//update the prev
 		soilMoistureDepthDayPrev = soilMoistureDepthDay;
 		waterVolDayPrev = waterVolDay;
 
-			//REST OF THE ALGORITHM IS WRITING TO FILES.
-		}
+	//Write out daily outputs:
+	//Write Date, InflowVolDay, EvaporationVolDay, SeepageVolDay, IrrigationVolDay, BypassVolDay, PondWaterDepthDay
+
+/*******Calculate annual values*******/
+var inflowVolYear = inflowVolYear + inflowVolDay;	//inflowVolYear should not be in its defintion.
+var evapVolYear = evapVolYear + evapVolDay; 		//again, evapVolYear should not be in its definition.
+var seepageVolYear = seepageVolYear + seepageVolDay;	//I thought this was constant.
+var irrigationVolYear = irrigationVolYear + irrigationVolDay;	//I'm assuming all of these are for the year-1
+var bypassVolYear = bypassVolYear + bypassVolDay
+var deficitVolYear = deficitVolYear + (deficitVolDay*pondArea);
+/*
+#for storage deficit, daily is as a depth, yearly is as a volume. 
+*/
+
+}
 
 
-	}
+
+
+}
