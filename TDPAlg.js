21ff05ca4a25b6601a6d2e844ac7d0b6b6bca303
marsha88 <marsha88@purdue.edu>
2017-02-18 22:33:39 -0500

    updated other vars

--- TDPAlg.js
+++ TDPAlg.js
@@ -5,15 +5,16 @@ Notes: This file exports the algorithm developed by the Transforming Drainage Pr
  */
 
 module.exports = function(pondVolSmallest, pondVolLargest, pondVolIncrement, pondDepth, maxSoilMoisture, irrigationArea, irrigationDepth){		//TODO: add last argument.
-	const seepageVolumeDay = 0.01; //ft
+	const seepageVolDay = 0.01; //ft
 	const numberOfIncrements = (pondVolLargest - pondVolSmallest)/pondVolIncrement;		//specify on front-end that the increment can't be zero.
 
 	for(i = 0; i < numberOfIncrements; i++){
-		var pondVolume = pondVolSmallest + (i * pondVolIncrement);
-		var pondArea = pondVolume/pondDepth;						//specify on front-end that the pondDepth can't be zero.
+		var pondVol = pondVolSmallest + (i * pondVolIncrement);
+		var pondArea = pondVol/pondDepth;						//specify on front-end that the pondDepth can't be zero.
 
 
 		var soilMoistureDepthDayPrev = maxSoilMoisture;
+		var waterVolDayPrev = //TODO: what should the default here be?
 		//The following should be done for every year, month, day and requires inputs from the database.
 
 		var precipDepthDay = //from database;
@@ -22,36 +23,44 @@ module.exports = function(pondVolSmallest, pondVolLargest, pondVolIncrement, pon
 		var waterVolDay = //from db;
 		var pondPrecipVolDay = percipDay * pondArea;
 		var soilMoistureDepthDay = soilMoistureDepthDayPrev + precipDepthDay – evapDepthDay;	//TODO: still waiting on frankenb to tell me if this is the correct statement.
+		var deficitVolDay;
+		var inflowVolDay = //from db;
+
 		if(soilMoistureDepthDay < 0.5*maxSoilMoisture){
 			irrigationVolDay = irrigationDepth * irrigationArea;
 
 			//CHECK WITH FRANKENB TO MAKE SURE THIS IF STATEMENT SHOULD BE NESTED.
 			if(irrigationVolDay > waterVolDay){
-				DeficitVolDay = (IrrigationVolDay – WaterVolDay)/PondArea;
-				else{
-					DeficitVolDay = 0;
-				}
+				deficitVolDay = (IrrigationVolDay – WaterVolDay)/PondArea;
 			}
+			else{
+				deficitVolDay = 0;
+			}
+
 		}
 
 		//I DO NOT KNOW WHAT SHE WAS GOING FOR HERE.
 		Read EvaporationVolDay //read from regional input file?????
+		//what is the difference between evapVolDay and evapDepthDay????
+		var waterVolDay = waterVolDayPrev + inflowVolDay + pondPrecipVolDay - irrigationVolDay - seepageVolDay – evapVolDay;
+		var bypassFlow, bypassFlowVolDay; 	//what is the difference here or are they the same?
 
-			WaterVolDay = WaterVolDay(date-1) + InflowVolDay +PondPrecipVolDay - IrrigationVolDay - SeepageVolDay – EvaporationVolDay 
-			if(WaterVolDay > PondVol){
-				BypassFlowVolDay= WaterVolDay – PondVol, 
-					WaterVolDay = PondVol
-			}
-			else{
-				BypassFlow=0
-			}
-		PondWaterDepthDay = WaterVolDay/PondArea	
+		if(WaterVolDay > PondVol){
+			bypassFlowVolDay= waterVolDay – pondVol;
+			waterVolDay = pondVol;
+		}
+		else{
+			bypassFlow=0
+		}
+
+		pondWaterDepthDay = waterVolDay/pondArea;
 
 		//update soilMoistureDepthDayPrev before loop ends
 		soilMoistureDepthDayPrev = soilMoistureDepthDay;
+		waterVolDayPrev = waterVolDay;
 
 			//REST OF THE ALGORITHM IS WRITING TO FILES.
+		}
+
+
 	}
-
-
-}
