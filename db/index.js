8e4c27883faaf156a29a5bccb45dd55d633a05a3
Drew Atkinson <drewatkinson5@gmail.com>
2017-02-24 17:48:44 -0500

    add docs

--- db/index.js
+++ db/index.js
@@ -9,6 +9,18 @@ nconf.file({
 var connection = mysql.createConnection(nconf.get('mysql'));
 connection.connect();
 
+/**
+ *  getLocationById -  Gets a Location's data given a location id
+ *
+ *  Return - Array of Rows -
+ *  [{
+ *    RecordedDate: 1980-10-05T04:00:00.000Z,
+ *    Drainflow: 0,
+ *    Precipitation: 0,
+ *    PET: 4.5055
+ *  }]
+ *
+ */
 exports.getLocationById = function(Id) {
   return new Promise(function(resolve, reject) {
     if (!Number.isInteger(Id)) {
@@ -23,12 +35,22 @@ exports.getLocationById = function(Id) {
   });
 };
 
-exports.getPETById = function(Id) {
+/**
+ *  getLocationById -  Gets a Location's evaporation data given a location id
+ *
+ *  Return - Array of Rows -
+ *  [{
+ *    RecordedDate: 1980-10-05T04:00:00.000Z,
+ *    PET: 4.5055
+ *  }]
+ *
+ */
+getPETById = function(Id) {
   return new Promise(function(resolve, reject) {
     if (!Number.isInteger(Id)) {
       reject(new Error('Location Id must be a number'));
     }
-    connection.query('SELECT PET FROM ??', 'Location' + Id, function(error, results, fields) {
+    connection.query('SELECT RecordedDate, PET FROM ??', 'Location' + Id, function(error, results, fields) {
       if (error) {
         reject(error);
       }
