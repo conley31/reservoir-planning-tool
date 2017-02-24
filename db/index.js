751741d53173597a4a658bc902e6370018e02265
Jonah Heeren <jonah.heeren@me.com>
2017-02-24 16:50:31 -0500

    Initial function haven't tested yet

--- db/index.js
+++ db/index.js
@@ -22,3 +22,19 @@ exports.getLocationById = function(Id) {
     });
   });
 };
+
+//Dates are expected as string in format YYYY-MM-DD
+exports.getLocationForDateRange = function(Id, startDate, endDate) {
+  connection.connect();
+  return new Promise(function(resolve, reject) {
+    if(!Number.isInteger(Id) || NaN == Date.parse(startDate) || NaN == Date.parse(endDate)) {
+      reject(new Error('Type Error: Expected Types are Int, Date as Str, Date as Str'));
+    }
+    connection.query('SELECT * FROM ?? WHERE RecordedDate >= \'?\' AND RecordedDate <= \'?\'', ['Location' + Id, startDate, endDate]function(error, results, fields) {
+      if (error) {
+        reject(error);
+      }
+      resolve(results);
+    });
+  });
+};
