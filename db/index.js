309c697efb67bc682ec5d20a1136a24e3fa2b536
Drew Atkinson <drewatkinson5@gmail.com>
2017-02-24 17:40:37 -0500

    add getPETByID

--- db/index.js
+++ db/index.js
@@ -7,14 +7,28 @@ nconf.file({
 });
 
 var connection = mysql.createConnection(nconf.get('mysql'));
+connection.connect();
 
 exports.getLocationById = function(Id) {
-  connection.connect();
   return new Promise(function(resolve, reject) {
     if (!Number.isInteger(Id)) {
       reject(new Error('Location Id must be a number'));
     }
     connection.query('SELECT * FROM ??', 'Location' + Id, function(error, results, fields) {
+      if (error) {
+        reject(error);
+      }
+      resolve(results);
+    });
+  });
+};
+
+exports.getPETById = function(Id) {
+  return new Promise(function(resolve, reject) {
+    if (!Number.isInteger(Id)) {
+      reject(new Error('Location Id must be a number'));
+    }
+    connection.query('SELECT PET FROM ??', 'Location' + Id, function(error, results, fields) {
       if (error) {
         reject(error);
       }
