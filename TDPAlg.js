f2d1476a6835c3dd75240f04e2178aacef2050d2
Drew Atkinson <drewatkinson5@gmail.com>
2017-03-02 18:43:11 -0500

    format TDPAlg.js

--- TDPAlg.js
+++ TDPAlg.js
@@ -8,142 +8,137 @@ Notes:
 var db = require('./db');
 
 module.exports.calc = function(_pondVolSmallest, _pondVolLargest, _pondVolIncrement, _pondDepth, _pondDepthInitial, _maxSoilMoisture, _irrigationArea, _irrigationDepth, _availableWaterCapacity, _locationId) { //TODO: add last argument.
-	return new Promise(function(resolve, reject){
-		db.getLocationById(_locationId).then(function(data) {
-			var numOfRows = data.length;
-			var allAnnuals = [];
-			const seepageVolDay = 0.01;
-			const numberOfIncrements = (_pondVolLargest - _pondVolSmallest) / _pondVolIncrement;
+  return new Promise(function(resolve, reject) {
+    db.getLocationById(_locationId).then(function(data) {
+      var numOfRows = data.length;
+      var allAnnuals = [];
+      const seepageVolDay = 0.01;
+      const numberOfIncrements = (_pondVolLargest - _pondVolSmallest) / _pondVolIncrement;
 
-	for (var i = 0; i < numberOfIncrements; i++) {
-		var pondVol = _pondVolSmallest + (i * _pondVolIncrement);
-		var pondArea = pondVol / _pondDepth;
-    /*
-    **********************************************
-    			     ANNUAL VALUES
-    **********************************************
-    */
-
-    var inflowVolYear = 0;
-    var evapVolYear = 0;
-    var seepageVolYear = 0;
-    var irrigationVolYear = 0;
-    var bypassFlowVolYear = 0;
-    var deficitVolYear = 0;
-
-
-      /*
-      **********************************************
-      			     DAY-1 VALUES
-      ***********************************************
-      */
-      var soilMoistureDepthDayPrev = _maxSoilMoisture;
-      var pondWaterVolDayPrev = _pondDepthInitial * pondArea;
-
-      /* LOOP THROUGH EVERY DAY */
-
-    for (var j = 0; j < data.length; j++) {
-    /*
-    **********************************************
-    			     DAILY VALUES
-    **********************************************
-    */
-
-    var inflowVolDay = data[j].Drainflow;
-    var precipDepthDay = data[j].Precipitation;
-    var evapDepthDay = data[j].PET;
-
-    var irrigationVolDay = 0;
-    var deficitVolDay;
-    var bypassFlowVolDay;
-
-    var evapVolDay = evapDepthDay * pondArea;
-
-    var pondPrecipVolDay = (precipDepthDay * pondArea);
-    var soilMoistureDepthDay = (soilMoistureDepthDayPrev + precipDepthDay - evapDepthDay);
-    var pondWaterVolDay;
-
-
-    if (soilMoistureDepthDay < (0.5 * _availableWaterCapacity)) {
-    	irrigationVolDay = _irrigationDepth * _irrigationArea;
-    	pondWaterVolDay = (pondWaterVolDayPrev + inflowVolDay + pondPrecipVolDay - irrigationVolDay - seepageVolDay - evapVolDay);
-
-    	if (irrigationVolDay > pondWaterVolDay) {
-    		deficitVolDay = (irrigationVolDay - pondWaterVolDay) / pondArea;
-    	} else {
-    		deficitVolDay = 0;
-    	}
-    }
-        //set pondWaterVolDay with an irrigationVolDay of zero.
-        else {
-        	deficitVolDay = 0;
-        	pondWaterVolDay = (pondWaterVolDayPrev + inflowVolDay + pondPrecipVolDay - irrigationVolDay - seepageVolDay - evapVolDay);
-        }
-
-        if (pondWaterVolDay > pondVol) {
-        	bypassFlowVolDay = pondWaterVolDay - pondVol;
-        	pondWaterVolDay = pondVol;
-        } 
-
-        else {
-        	bypassFlowVolDay = 0;
-        }
-
-        var pondWaterDepthDay = pondWaterVolDay / pondArea;
-
+      for (var i = 0; i < numberOfIncrements; i++) {
+        var pondVol = _pondVolSmallest + (i * _pondVolIncrement);
+        var pondArea = pondVol / _pondDepth;
         /*
-        **************************************************************************************************************
-
-        	  							WRITE OUT ALL DAILY INFORMATION HERE.
-
-        (Write Date, InflowVolDay, EvaporationVolDay, SeepageVolDay, IrrigationVolDay, BypassVolDay, PondWaterDepthDay)
-
-        ***************************************************************************************************************
+        **********************************************
+        			     ANNUAL VALUES
+        **********************************************
         */
 
-        //update the (day-1) variables
-        soilMoistureDepthDayPrev = soilMoistureDepthDay;
-        pondWaterVolDayPrev = pondWaterVolDay;
+        var inflowVolYear = 0;
+        var evapVolYear = 0;
+        var seepageVolYear = 0;
+        var irrigationVolYear = 0;
+        var bypassFlowVolYear = 0;
+        var deficitVolYear = 0;
+
 
         /*
-        **************************************************************************************************************
-
-        			  THIS MARKS THE END OF THE DAILY COUNTS. UPDATE THE ANNUAL VALUES.
-
-        ***************************************************************************************************************
+        **********************************************
+        			     DAY-1 VALUES
+        ***********************************************
         */
+        var soilMoistureDepthDayPrev = _maxSoilMoisture;
+        var pondWaterVolDayPrev = _pondDepthInitial * pondArea;
+
+        /* LOOP THROUGH EVERY DAY */
+
+        for (var j = 0; j < data.length; j++) {
+          /*
+          **********************************************
+          			     DAILY VALUES
+          **********************************************
+          */
+
+          var inflowVolDay = data[j].Drainflow;
+          var precipDepthDay = data[j].Precipitation;
+          var evapDepthDay = data[j].PET;
+
+          var irrigationVolDay = 0;
+          var deficitVolDay;
+          var bypassFlowVolDay;
+
+          var evapVolDay = evapDepthDay * pondArea;
+
+          var pondPrecipVolDay = (precipDepthDay * pondArea);
+          var soilMoistureDepthDay = (soilMoistureDepthDayPrev + precipDepthDay - evapDepthDay);
+          var pondWaterVolDay;
 
 
-        /*******Calculate annual values*******/
+          if (soilMoistureDepthDay < (0.5 * _availableWaterCapacity)) {
+            irrigationVolDay = _irrigationDepth * _irrigationArea;
+            pondWaterVolDay = (pondWaterVolDayPrev + inflowVolDay + pondPrecipVolDay - irrigationVolDay - seepageVolDay - evapVolDay);
 
-        inflowVolYear += inflowVolDay;
-        evapVolYear += evapVolDay;
-        seepageVolYear += seepageVolDay;
-        irrigationVolYear += irrigationVolDay;
-        bypassFlowVolYear += bypassFlowVolDay;
-        deficitVolYear += (deficitVolDay * pondArea);
-    /*
-    **************************************************************************************************************
+            if (irrigationVolDay > pondWaterVolDay) {
+              deficitVolDay = (irrigationVolDay - pondWaterVolDay) / pondArea;
+            } else {
+              deficitVolDay = 0;
+            }
+          }
+          //set pondWaterVolDay with an irrigationVolDay of zero.
+          else {
+            deficitVolDay = 0;
+            pondWaterVolDay = (pondWaterVolDayPrev + inflowVolDay + pondPrecipVolDay - irrigationVolDay - seepageVolDay - evapVolDay);
+          }
 
-    			  				   WRITE OUT ALL ANNUAL INFORMATION HERE.
-    (Write Date, InflowVolYear, EvaporationVolYear, SeepageVolYear, IrrigationVolYear, BypassVolYear, PondWaterDepthYear)
+          if (pondWaterVolDay > pondVol) {
+            bypassFlowVolDay = pondWaterVolDay - pondVol;
+            pondWaterVolDay = pondVol;
+          } else {
+            bypassFlowVolDay = 0;
+          }
 
-    ***************************************************************************************************************
-    */
-}
+          var pondWaterDepthDay = pondWaterVolDay / pondArea;
 
-allAnnuals.push([pondVol, (inflowVolYear / numOfRows), (evapVolYear / numOfRows), (seepageVolDay / numOfRows), (irrigationVolYear / numOfRows), (bypassFlowVolYear / numOfRows), (deficitVolYear / numOfRows)]);
+          /*
+          **************************************************************************************************************
 
-}
+          	  							WRITE OUT ALL DAILY INFORMATION HERE.
 
-resolve (allAnnuals);
+          (Write Date, InflowVolDay, EvaporationVolDay, SeepageVolDay, IrrigationVolDay, BypassVolDay, PondWaterDepthDay)
 
-});
+          ***************************************************************************************************************
+          */
 
-});
+          //update the (day-1) variables
+          soilMoistureDepthDayPrev = soilMoistureDepthDay;
+          pondWaterVolDayPrev = pondWaterVolDay;
+
+          /*
+          **************************************************************************************************************
+
+          			  THIS MARKS THE END OF THE DAILY COUNTS. UPDATE THE ANNUAL VALUES.
+
+          ***************************************************************************************************************
+          */
+
+
+          /*******Calculate annual values*******/
+
+          inflowVolYear += inflowVolDay;
+          evapVolYear += evapVolDay;
+          seepageVolYear += seepageVolDay;
+          irrigationVolYear += irrigationVolDay;
+          bypassFlowVolYear += bypassFlowVolDay;
+          deficitVolYear += (deficitVolDay * pondArea);
+          /*
+          **************************************************************************************************************
+
+          			  				   WRITE OUT ALL ANNUAL INFORMATION HERE.
+          (Write Date, InflowVolYear, EvaporationVolYear, SeepageVolYear, IrrigationVolYear, BypassVolYear, PondWaterDepthYear)
+
+          ***************************************************************************************************************
+          */
+        }
+
+        allAnnuals.push([pondVol, (inflowVolYear / numOfRows), (evapVolYear / numOfRows), (seepageVolDay / numOfRows), (irrigationVolYear / numOfRows), (bypassFlowVolYear / numOfRows), (deficitVolYear / numOfRows)]);
+
+      }
+
+      resolve(allAnnuals);
+
+    });
+
+  });
 
 
 };
-
-
-
