3119d2a6b09c44b64cf5deda501656186b7c344e
Drew Atkinson <drewatkinson5@gmail.com>
2017-03-03 09:42:16 -0500

    add back polygons.js

---
+++ polygons.js
@@ -0,0 +1,32 @@
+_ = require('lodash');
+
+polygons = require('./public/final_index_FeaturesToJSON.json');
+
+// Re-format array
+var featuresArray = _.map(polygons.features, f => {
+  return {
+    // Lower right-hand corner of the polygon
+    pos: {
+      lat: f.geometry.coordinates[0][0][1],
+      lng: f.geometry.coordinates[0][0][0]
+    },
+    id: f.properties.Id
+  };
+});
+
+var features = _.groupBy(featuresArray, f => {
+  return f.pos.lng;
+});
+
+for (var i = 0; i < features.length; i++) {
+  features[i] = _.sortBy(features[i], f => {
+    return f.pos.lat;
+  });
+}
+
+
+exports.getFeatures = () => {
+  return features;
+};
+
+console.log(features);
