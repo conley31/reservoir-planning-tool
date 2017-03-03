8fae28e21399ed9dd8356116ed35e819e9c7c8c9
Jonah Heeren <jonah.heeren@me.com>
2017-03-02 22:05:10 -0500

    Dot Notation for Arrays

--- UserParse.js
+++ UserParse.js
@@ -4,25 +4,25 @@ var db = require('./db');
 
 var dateTrack = null;
 
-module.exports.verifyAndBlendUserCSV = function(id, inStream) {
+function verifyAndBlendUserCSV(id, inStream) {
   return new Promise(function(resolve, reject) {
     var buffer = [];
     var blendedArray = [];
     csv
     .fromStream(inStream, {headers : ["Year", "Month", "Day", "Drainflow", "Precipitation", "PET"]})
     .validate(function(data) {
-      return (isValidDate(data["Day"], data["Month"], data["Year"]) &&
-              isValidNumber(data["Drainflow"]) &&
-              isValidNumber(data["Precipitation"]) &&
-              isValidNumber(data["PET"]));
+      return (isValidDate(data.Day, data.Month, data.Year) &&
+              isValidNumber(data.Drainflow) &&
+              isValidNumber(data.Precipitation) &&
+              isValidNumber(data.PET));
     })
     .on("data-invalid", function(data, index) {
-      reject('Line ' + (index + 1) + ': ' + data["Year"]
-                      + ',' + data["Month"]
-                      + ',' + data["Day"]
-                      + ',' + data["Drainflow"]
-                      + ',' + data["Precipitation"]
-                      + ',' + data["PET"]);
+      reject('Line ' + (index + 1) + ': ' + data.Year
+                      + ',' + data.Month
+                      + ',' + data.Day
+                      + ',' + data.Drainflow
+                      + ',' + data.Precipitation
+                      + ',' + data.PET);
     })
     .on("data", function(data) {
       buffer.push(data);
@@ -38,6 +38,7 @@ module.exports.verifyAndBlendUserCSV = function(id, inStream) {
           blendedArray = blendedArray.concat(response[1]);
         }
         blendedArray.push(buffer[buffer.length - 1]);
+        console.log(blendedArray);
       });
     });
   });
@@ -46,11 +47,11 @@ module.exports.verifyAndBlendUserCSV = function(id, inStream) {
 
 function fillGaps(startIndex, sqlRows, userRowStart, userRowEnd) {
   var index = startIndex;
-  startDate = new Date(userRowStart["Year"], userRowStart["Month"], userRowStart["Day"]);
-  endDate = new Date(userRowEnd["Year"], userRowEnd["Month"], userRowEnd["Day"]);
+  startDate = new Date(userRowStart.Year, userRowStart.Month, userRowStart.Day);
+  endDate = new Date(userRowEnd.Year, userRowEnd.Month, userRowEnd.Day);
   var arr = [];
-  while(index < sqlRows.length && sqlRows[index]["RecordedDate"] < endDate) {
-    if(sqlRows[index]["RecordedDate"] > startDate) {
+  while(index < sqlRows.length && sqlRows[index].RecordedDate < endDate) {
+    if(sqlRows[index].RecordedDate > startDate) {
       arr.push(formattedHash(sqlRows[index]));
     }
     ++index;
@@ -59,18 +60,18 @@ function fillGaps(startIndex, sqlRows, userRowStart, userRowEnd) {
 }
 
 function formattedHash(sqlRow) {
-  var row = {"Year": sqlRow["RecordedDate"].getFullYear().toString(),
-             "Month": (sqlRow["RecordedDate"].getMonth()).toString(),
-             "Day": sqlRow["RecordedDate"].getDate().toString(),
-             "Drainflow": sqlRow["Drainflow"],
-             "Precipitation": sqlRow["Precipitation"],
-             "PET": sqlRow["PET"]}
+  var row = {"Year": sqlRow.RecordedDate.getFullYear().toString(),
+             "Month": (sqlRow.RecordedDate.getMonth()).toString(),
+             "Day": sqlRow.RecordedDate.getDate().toString(),
+             "Drainflow": sqlRow.Drainflow,
+             "Precipitation": sqlRow.Precipitation,
+             "PET": sqlRow.PET}
   return row;
 }
 
 function seek(rows, firstbuf) {
   var i = 0;
-  while(rows[i]["RecordedDate"] < new Date(firstbuf["Year"], firstbuf["Month"] - 1, firstbuf["Day"]))
+  while(rows[i].RecordedDate < new Date(firstbuf.Year, firstbuf.Month - 1, firstbuf.Day))
     i++;
   return ++i;
 }
@@ -117,3 +118,6 @@ Date.prototype.toShortString = function() {
 
   return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
 }
+
+var inStream = fs.createReadStream("db/daily_files/Daily_48.9375_-99.1875.txt");
+var rows = verifyAndBlendUserCSV(4, inStream);
