cfd4c47d3fdd491151af5abb49d8a25d0f9c861c
marsha88 <marsha88@purdue.edu>
2017-03-11 15:45:44 -0500

    updated total calculation according to march 9th doc.

--- TDPAlg.js
+++ TDPAlg.js
@@ -10,20 +10,19 @@ var db = require('./db');
 module.exports.calc = function(_pondVolSmallest, _pondVolLargest, _pondVolIncrement, _pondDepth, _pondDepthInitial, _maxSoilMoisture, _irrigationArea, _irrigationDepth, _availableWaterCapacity, _locationId) { //TODO: add last argument.
   return new Promise(function(resolve, reject) {
     db.getLocationById(_locationId).then(function(data) {
-      var numOfRows = data.length;
+      var numOfDays = data.length;
       var allAnnuals = [];
-      const seepageVolDay = 0.01;
-      const numberOfIncrements = (_pondVolLargest - _pondVolSmallest) / _pondVolIncrement;
+      const seepageVolDay = 0.01; //feet
+      const numberOfIncrements = ((_pondVolLargest - _pondVolSmallest) / _pondVolIncrement);
 
       for (var i = 0; i < numberOfIncrements; i++) {
         var pondVol = _pondVolSmallest + (i * _pondVolIncrement);
         var pondArea = pondVol/_pondDepth;
         /*
         **********************************************
-        			     ANNUAL VALUES
+        			     TOTAL VALUES
         **********************************************
         */
-
         var inflowVolTotal = 0;
         var evapVolTotal = 0;
         var seepageVolTotal = 0;
@@ -31,14 +30,13 @@ module.exports.calc = function(_pondVolSmallest, _pondVolLargest, _pondVolIncrem
         var bypassFlowVolTotal = 0;
         var deficitVolTotal = 0;
 
-
         /*
         **********************************************
         			     DAY-1 VALUES
         ***********************************************
         */
-        var soilMoistureDepthDayPrev = _maxSoilMoisture;
-        var pondWaterVolDayPrev = _pondDepthInitial * pondArea;
+        var soilMoistureDepthDayPrev = _maxSoilMoisture;	//inches
+        var pondWaterVolDayPrev = _pondDepthInitial * pondArea; //acre-feet
 
         /* LOOP THROUGH EVERY DAY */
 
@@ -48,56 +46,42 @@ module.exports.calc = function(_pondVolSmallest, _pondVolLargest, _pondVolIncrem
           			     DAILY VALUES
           **********************************************
           */
-
           var inflowVolDay = data[j].Drainflow;
           var precipDepthDay = data[j].Precipitation;
           var evapDepthDay = data[j].PET;
 
           var irrigationVolDay = 0;
-          var deficitVolDay;
+          var deficitVolDay = 0;
           var bypassFlowVolDay;
 
           var evapVolDay = evapDepthDay * pondArea;
 
           var pondPrecipVolDay = (precipDepthDay * pondArea);
           var soilMoistureDepthDay = (soilMoistureDepthDayPrev + precipDepthDay - evapDepthDay);
-          var pondWaterVolDay;
 
 
           if (soilMoistureDepthDay < (0.5 * _availableWaterCapacity)) {
             irrigationVolDay = _irrigationDepth * _irrigationArea;
-            pondWaterVolDay = (pondWaterVolDayPrev + inflowVolDay + pondPrecipVolDay - irrigationVolDay - seepageVolDay - evapVolDay);
 
             if (irrigationVolDay > pondWaterVolDay) {
-              deficitVolDay = (irrigationVolDay - pondWaterVolDay) / pondArea;
-            } else {
-              deficitVolDay = 0;
-            }
+              deficitVolDay = (irrigationVolDay - pondWaterVolDay);
+          	}
           }
-          //set pondWaterVolDay with an irrigationVolDay of zero.
-          else {
-            deficitVolDay = 0;
-            pondWaterVolDay = (pondWaterVolDayPrev + inflowVolDay + pondPrecipVolDay - irrigationVolDay - seepageVolDay - evapVolDay);
-          }
+          //update water volume in pond
+          var pondWaterVolDay = (pondWaterVolDayPrev + inflowVolDay + pondPrecipVolDay - irrigationVolDay - seepageVolDay - evapVolDay);
 
+          //calculate bypass
           if (pondWaterVolDay > pondVol) {
             bypassFlowVolDay = pondWaterVolDay - pondVol;
             pondWaterVolDay = pondVol;
-          } else {
+          } 
+
+          else {
             bypassFlowVolDay = 0;
           }
 
           var pondWaterDepthDay = pondWaterVolDay/pondArea;
 
-          /*
-          **************************************************************************************************************
-
-          	  							WRITE OUT ALL DAILY INFORMATION HERE.
-
-          (Write Date, InflowVolDay, EvaporationVolDay, SeepageVolDay, IrrigationVolDay, BypassVolDay, PondWaterDepthDay)
-
-          ***************************************************************************************************************
-          */
 
           //update the (day-1) variables
           soilMoistureDepthDayPrev = soilMoistureDepthDay;
@@ -112,7 +96,7 @@ module.exports.calc = function(_pondVolSmallest, _pondVolLargest, _pondVolIncrem
           */
 
 
-          /*******Calculate annual values*******/
+          /*******Calculate totals for every day in database*******/
 
           inflowVolTotal += inflowVolDay;
           evapVolTotal+= evapVolDay;
@@ -120,18 +104,11 @@ module.exports.calc = function(_pondVolSmallest, _pondVolLargest, _pondVolIncrem
           irrigationVolTotal+= irrigationVolDay;
           bypassFlowVolTotal += bypassFlowVolDay;
           deficitVolTotal += (deficitVolDay * pondArea);
-          /*
-          **************************************************************************************************************
-
-          			  				   WRITE OUT ALL ANNUAL INFORMATION HERE.
-          (Write Date, InflowVolYear, EvaporationVolYear, SeepageVolYear, IrrigationVolYear, BypassVolYear, PondWaterDepthYear)
-
-          ***************************************************************************************************************
-          */
         }
-
+        /***** THE COMMENTED REGION BELOW CONTAINS AVGS FOR ALL VALUES ******/
         //allAnnuals.push([pondVol, (inflowVolYear / numOfRows), (evapVolYear / numOfRows), (seepageVolDay / numOfRows), (irrigationVolYear / numOfRows), (bypassFlowVolYear / numOfRows), (deficitVolYear / numOfRows)]);
-        allAnnuals.push([pondVol, (bypassFlowVolYear / numOfRows), (deficitVolYear / numOfRows)]);
+
+        allAnnuals.push([pondVol, (bypassFlowVolYear / numOfDays), (deficitVolYear / numOfDays)]);
 
       }
 
