f2210c8a9bc9f4328425a315cdb322cef8b4ef8d
Jonah Heeren <jonah.heeren@me.com>
2017-03-26 00:14:46 -0400

    Test table first

--- db/tests/test_setup.py
+++ db/tests/test_setup.py
@@ -23,6 +23,20 @@ with open('../index.csv', 'rb') as csvfile:
     for row in stream:
         index_rows.append(row)
 
+def assertAlmostEqual(a, b):
+    if abs(a - b) < 0.00001:
+        return True
+    return False
+
+def compare_rows(test_row, rows):
+    for row in rows:
+        if test_row[0] == row[0]:
+            assertAlmostEqual(test_row[1], row[1])
+            assertAlmostEqual(test_row[2], row[2])
+            assertAlmostEqual(test_row[3], row[3])
+            return True
+    return False
+
 def get_table_name(input):
     return input[0]
 
@@ -41,8 +55,6 @@ def test_tables_equal():
 def test_table_first():
     cur.execute(sql_statements.select_from.format(str('Location' + index_rows[0][1])))
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
     with open('../daily_files/' + index_rows[0][4], 'rb') as csvfile:
       stream = csv.reader(csvfile, delimiter=',')
       for row in stream:
@@ -51,5 +63,4 @@ def test_table_first():
           precipitation = float('%.6f'%(INCH_FACTOR * float(row[4])))
           PET = float('%.6f'%(INCH_FACTOR * float(row[5])))
           test_row = (date, drainflow, precipitation, PET)
-          print(test_row)
-          assert(test_row in rows)
+          assert(compare_rows(test_row, rows))
