60e3d8d0aed7ffaa3bab381d4169cfe5df28fd8d
Jonah Heeren <jonah.heeren@me.com>
2017-03-26 02:58:28 -0400

    Setup for update testing

--- db/tests/test_update.py
+++ db/tests/test_update.py
@@ -1,9 +1,13 @@
 import csv, json, datetime, os
 from nose.tools import assert_equals
 from nose.plugins.attrib import attr
-from db import sql_statements, update
+from db import sql_statements, update, setupdb
 import MySQLdb as db
 
 def test_to_str_date():
     str_date = update.toStrDate('1995', '10', '24')
     assert_equals(str_date, '1995-10-24')
+
+def test_db_create():
+    setupdb.database = 'testTDP'
+    setupdb.index_file = 'test/index.csv'
