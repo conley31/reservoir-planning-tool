d7927e50e91bac2c2b32a04dd28d1dd5473a51d2
Jonah Heeren <jonah.heeren@me.com>
2017-04-02 21:40:01 -0400

    LocationByIDTests

---
+++ tests/DBExportTests/QueryTests.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+var chaiAsPromised = require("chai-as-promised");
+var db = require('../../db');
+
+chai.use(chaiAsPromised);
+
+it('should get valid location data from ID', function() { // no done
+    return db.getLocationById(5).then(function(data){
+        expect(data).to.be.an('array');
+    });
+});
+
+it('should be rejected for an invalid ID', function() { // no done
+    return assert.isRejected(db.getLocationById(-23), "ER_NO_SUCH_TABLE: Table 'tdp.location-23' doesn't exist");
+});
+
+it('should require an integer ID', function() { // no done
+    return assert.isRejected(db.getLocationById('23'), "Location Id must be a number");
+});
