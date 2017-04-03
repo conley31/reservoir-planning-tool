fec2ab5e2c4a33649fc74f409d93094b720c9113
Jonah Heeren <jonah.heeren@me.com>
2017-04-03 19:09:55 -0400

    Added date range tests

--- tests/DBExportTests/QueryTests.js
+++ tests/DBExportTests/QueryTests.js
@@ -22,8 +22,22 @@ it('should require an integer ID', function() { // no done
 
 it('should get RecordedDate & PET from ID', function() { // no done
     return db.getPETById(5).then(function(data){
-        console.log(db.getPETById(5));
         expect(data).to.be.an('array');
         expect(data[0]).to.have.all.keys('RecordedDate', 'PET');
     });
+});
+
+it('should get data within a date range for an ID', function() { // no done
+  return db.getLocationForDateRange(5, '1981-01-01', '1990-01-01').then(function(data){
+      for(i = 0; i < data.length; i++) {
+        expect(data[i].RecordedDate).to.satisfy(function(date) { return date > Date.parse('1980-12-31'); });
+        expect(data[i].RecordedDate).to.satisfy(function(date2) { return date2 < Date.parse('1990-01-02'); });
+      }
+  });
+});
+
+it('should have a 0 length for data out of range', function() { // no done
+  return db.getLocationForDateRange(5, '1781-01-01', '1790-01-01').then(function(data){
+      expect(data).to.be.empty;
+  });
 });
