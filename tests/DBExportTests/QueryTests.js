48340959a4cd968abab05c7dc33cc25619e266bb
Jonah Heeren <jonah.heeren@me.com>
2017-04-02 21:57:06 -0400

    Testing for PETByID

--- tests/DBExportTests/QueryTests.js
+++ tests/DBExportTests/QueryTests.js
@@ -19,3 +19,11 @@ it('should be rejected for an invalid ID', function() { // no done
 it('should require an integer ID', function() { // no done
     return assert.isRejected(db.getLocationById('23'), "Location Id must be a number");
 });
+
+it('should get RecordedDate & PET from ID', function() { // no done
+    return db.getPETById(5).then(function(data){
+        console.log(db.getPETById(5));
+        expect(data).to.be.an('array');
+        expect(data[0]).to.have.all.keys('RecordedDate', 'PET');
+    });
+});
