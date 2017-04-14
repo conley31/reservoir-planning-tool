fa9f0b7f7453eb4c960250e45a582d01c4cb4277
vritant <vritoku@gmail.com>
2017-04-14 15:59:14 -0400

    styled error display

--- public/error_template.js
+++ public/error_template.js
@@ -1,11 +1,13 @@
 var displayFormError = function(array) {
   var errorContent = $("#error-content");
   if(array == undefined || array == null) {
-    $("#form-error").fadeOut('fast', function() {
+    $("#form-error").slideUp('fast', function() {
       errorContent.empty();
     });
   }
+  $("#form-error").slideDown('slow');
   errorContent.empty();
+  errorContent.append("<p>Please fix the following error(s)<p>");
   for(var i = 0; i < array.length; i++) {
     errorContent.append("<p> - " + array[i] + "</p>");
   }
