0f2c7928e7d56f09403e9e2e0d3d1c1e921df110
jonahheeren <jonah.heeren@me.com>
2017-04-19 19:50:12 -0400

    Fixes errors not going away

--- public/error_template.js
+++ public/error_template.js
@@ -5,18 +5,20 @@
 */
 var displayFormError = function(array) {
   var errorContent = $("#error-content");
-  if(array === undefined || array === null) {
+  if(array.length === 0) {
     $("#form-error").slideUp('fast', function() {
       errorContent.empty();
     });
   }
-  $("#form-error").slideDown('slow');
-  errorContent.empty();
-  if(array.length > 1)
-    errorContent.append("<p>Please fix the following errors:<p>");
-  else
-    errorContent.append("<p>Please fix the following error:<p>");
-  for(var i = 0; i < array.length; i++) {
-    errorContent.append("<p> - " + array[i] + "</p>");
+  else {
+    $("#form-error").slideDown('slow');
+    errorContent.empty();
+    if(array.length > 1)
+      errorContent.append("<p>Please fix the following errors:<p>");
+    else
+      errorContent.append("<p>Please fix the following error:<p>");
+    for(var i = 0; i < array.length; i++) {
+      errorContent.append("<p> - " + array[i] + "</p>");
+    }
   }
 };
