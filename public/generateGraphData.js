a3c70cf16858a258788e4ca3218f8128b01e026f
jonahheeren <jonah.heeren@me.com>
2017-04-23 15:06:35 -0400

    Implement findIndex and don't use arrow notation

--- public/generateGraphData.js
+++ public/generateGraphData.js
@@ -90,7 +90,7 @@ generateGraphData.allYearsByPondVolume = function(allYears, increments, specific
 	fillFour(outputArray);
 
 	/* Grab the index corresponding to the given pondVolume */
-	var currentIncrement = increments.findIndex((vol)=>{
+	var currentIncrement = increments.findIndex(function(vol) {
 		return vol === specificPondVolume;
 	});
 
@@ -126,7 +126,7 @@ generateGraphData.allMonthsByYear = function(allYears, increments, initialYear, 
 	var outputArray = new Array(12);	//this array will be contain an array for every month
 	fillFour(outputArray);
 
-	var currentIncrement = increments.findIndex((vol)=>{
+	var currentIncrement = increments.findIndex(function(vol) {
 		return vol === specificPondVolume;
 	});
 	var yearIndex = specificYear - initialYear;
@@ -152,3 +152,49 @@ generateGraphData.allMonthsByYear = function(allYears, increments, initialYear, 
 
 	return outputArray;
 };
+
+// Defines the implementation of findIndex for IE.
+// https://tc39.github.io/ecma262/#sec-array.prototype.findIndex
+if (!Array.prototype.findIndex) {
+  Object.defineProperty(Array.prototype, 'findIndex', {
+    value: function(predicate) {
+     // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return k.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return k;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return -1.
+      return -1;
+    }
+  });
+}
