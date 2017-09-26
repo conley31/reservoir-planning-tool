d4a3c11453958928a7e4ffe403a07f2740ec034f
Jordan Hagedorn <hagedorj@purdue.edu>
2017-09-25 20:13:26 -0400

    Implement algorithms.

--- newFeatures/algorithms.js
+++ newFeatures/algorithms.js
@@ -6,18 +6,12 @@ var gettables = require('./iterate-database');
 
 function cellData() {
      this.locationID = 0;
-     this.irrigationVolume = 0;
-     this.capturedFlow = 0;
-     this.drainFlow = 0;
+     this.cumulativeIrrigationVolume = 0;   //this member is not entirely necessary for the final product and can be replaced by a local variable
+     this.cumulativeCapturedFlow = 0;       //this member is not entirely necessary for the final product and can be replaced by a local variable
+     this.cumulativeDrainflow = 0;          //this member is not entirely necessary for the final product and can be replaced by a local variable
      this.annualIrrigationDepthSupplied = 0;
      this.percentAnnualCapturedDrainFlow = 0;
 }
-
-exports.calcAllLocations = function(drainedArea, initialDepth, irrigationIncrement, pondVol, soilMoisture, waterCapacity){
-    var tableCount = gettables.getNumberOfTables();
-    for (var i = 0; i < tableCount; i++) {
-        var locationId = ('Location' + i);
-        db.getLocationById(locationId).then(function(data){
             /* 
              *  'data' is an array object that corresponds to a table (AKA Location) inside the mysql database
              *  'data' indices are days (example: data[3] corresponds to LocationX's data for the 4th recorded date.
@@ -37,24 +31,28 @@ exports.calcAllLocations = function(drainedArea, initialDepth, irrigationIncreme
              *
              */
 
-             var cell = new cellData();
+exports.calcAllLocations = function(drainedArea, pondDepth, irrigationDepth, pondVol, soilMoisture, waterCapacity){
+    var tableCount = gettables.getNumberOfTables();
+
+    var allCells = [];
+
+    for (var i = 0; i < tableCount; i++) {
+        var locationId = ('Location' + i);
+        db.getLocationById(locationId).then(function(data){
+            
+             var cell = new cellData();     //create new cell to be inserted into the array. Each cell represents one location(grid cell).
              cell.locationID = locationId;
                 for(var j = 0; j < data.length; j++){
-                    
+                    cell.cumulativeIrrigationVolume += (irrigationDepth / 12 * drainedArea);
+
+                    cell.cumulativeCapturedFlow += (data[j].Drainflow / 12 * drainedArea);
+                    cell.cumulativeDrainflow += (data[j].Drainflow);
                 }
-
-            /*
-             * TODO
-             * add algorithms into the for loop above and store results in the calculatedData object
-             *
-             * Example:
-             * myData = new calculatedData();
-             * myData.annualDrainflow += data[j].Drainflow;
-             *
-             */
-
-
+             cell.annualIrrigationDepthSupplied = (cell.cumulativeIrrigationVolume * .15);
+             cell.percentAnnualCapturedDrainflow = (cell.cumulativeCapturedFlow / cell.cumulativeDrainflow);
+             allCells[i] = cell;
         }
         
     }
+    return allCells;
 }
