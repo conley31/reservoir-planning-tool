ecb13db30573d237eaa0742c9c686b093d0c19b8
Alexis <will1397@purdue.edu>
2017-10-04 14:34:53 -0400

    Add algorithm to create a json that holds all yearly data

--- newFeatures/parseJson.js
+++ newFeatures/parseJson.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var json = (fs.readFileSync("all-low-data.json"));
+var json = (fs.readFileSync("allData-16Vol-Low.json"));
 var response = JSON.parse(json);
 for(var i = 0; i < response.length; i++) {
 	console.log(response[i]);
