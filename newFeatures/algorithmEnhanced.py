1e596c695d18a504ea8775ffce688ede34179d94
Jordan Hagedorn <hagedorj@purdue.edu>
2017-10-19 22:37:06 -0400

    add sql statements to work with python code

--- newFeatures/algorithmEnhanced.py
+++ newFeatures/algorithmEnhanced.py
@@ -3,10 +3,10 @@
 import MySQLdb as db
 import imp
 import json
+from sql_statements import *
 
 with open('../config/config.json') as json_data:
   config = json.load(json_data)
-
 
 host = config.get('mysql').get('host')
 user = config.get('mysql').get('user')
@@ -14,13 +14,13 @@ password = config.get('mysql').get('password')
 database = config.get('mysql').get('database')
 log_location = config.get('mysql').get('logLocation')
 
-
 def getTableCount():
-  quer = '''SELECT count(*) AS tablesCount FROM information_schema.tables WHERE table_schema = ''' 
   con = db.connect(host,user,password,database)
   cur = con.cursor()
-  cur.execute(sql_statements.select_table_count.format(database))
+  cur.execute(select_table_count.format(database))
   for row in cur.fetchall():
     print row[0]
   con.close()
+
+getTableCount()
 
