4a64cec6b0a60fb2c4ced188336a0153a288fc56
Jordan Hagedorn <hagedorj@purdue.edu>
2017-10-27 09:01:24 -0400

    create JSON outfiles using multiprocessing/threading

--- newFeatures/createJsonData.py
+++ newFeatures/createJsonData.py
@@ -1,43 +1,35 @@
 #!/usr/bin/python
 
-import threading
-import time
-import Queue
+import json
 import algorithmEnhanced
+import multiprocessing
 
 #computeData(80,16,10,7.6,1,4.2)
 
-class MyThread(threading.Thread):
-  def __init__(self,area,volume,depth,soil,inc,water):
-    super(MyThread,self).__init__()
-    self.area=area
-    self.volume=volume
-    self.depth=depth
-    self.soil=soil
-    self.inc=inc
-    self.water=water
-  def run(self):
-    algorithmEnhanced.computeData(self.area,self.volume,self.depth,self.soil,self.inc,self.water)
+def worker(area,vol,depth,moisture,incr,water,num):
+  data = algorithmEnhanced.computeData(area,vol,depth,moisture,incr,water)
+  json_string = json.dumps([ob.__dict__ for ob in data])
+  text_file = open("output.txt"+str(num),"w")
+  text_file.write(json_string)
+  text_file.close()
 
-    #computeData(80,16,10,7.6,1,4.2)
-
-
-thread1 = MyThread(80,16,10,7.6,1,4.2)
-thread2 = MyThread(80,16,10,12,1,6.1)
-thread3 = MyThread(80,16,10,15.6,1,10.2)
-thread4 = MyThread(80,48,10,7.6,1,4.2)
-thread5 = MyThread(80,48,10,12,1,6.1)
-thread6 = MyThread(80,48,10,15.6,1,10.2)
-thread7 = MyThread(80,80,10,7.6,1,4.2)
-thread8 = MyThread(80,80,10,12,1,6.1)
-thread9 = MyThread(80,80,10,15.6,1,10.2)
-
-thread1.start()
-thread2.start()
-thread3.start()
-thread4.start()
-thread5.start()
-thread6.start()
-thread7.start()
-thread8.start()
-thread9.start()
+if __name__ == '__main__':
+  jobs = []
+  p1 = multiprocessing.Process(target=worker, args=(80,16,10,7.6,1,4.2,1))
+  p2 = multiprocessing.Process(target=worker, args=(80,16,10,12,1,6.1,2))
+  p3 = multiprocessing.Process(target=worker, args=(80,16,10,15.6,1,10.2,3))
+  p4 = multiprocessing.Process(target=worker, args=(80,48,10,7.6,1,4.2,4))
+  p5 = multiprocessing.Process(target=worker, args=(80,48,10,12,1,6.1,5))
+  p6 = multiprocessing.Process(target=worker, args=(80,48,10,15.6,1,10.2,6))
+  p7 = multiprocessing.Process(target=worker, args=(80,80,10,7.6,1,4.2,7))
+  p8 = multiprocessing.Process(target=worker, args=(80,80,10,12,1,6.1,8))
+  p9 = multiprocessing.Process(target=worker, args=(80,80,10,15.6,1,10.2,9))
+  p1.start()
+  p2.start()
+  p3.start()
+  p4.start()
+  p5.start()
+  p6.start()
+  p7.start()
+  p8.start()
+  p9.start()
