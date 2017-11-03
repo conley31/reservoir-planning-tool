3f1a105484ec88e10d362d9a4a53dc76ea087302
Jordan Hagedorn <hagedorj@purdue.edu>
2017-11-02 22:20:19 -0400

    successful generation of kml files, incomplete

--- newFeatures/geo-json/makekml.py
+++ newFeatures/geo-json/makekml.py
@@ -1,23 +1,40 @@
 import lxml
+import json
 from pykml import parser
 from lxml import etree
 
-json_data_files = ['allData-16Vol-Low', 'allData-16Vol-Medium', 'allData-16Vol-High',
-                   'allData-48Vol-Low', 'allData-48Vol-Medium', 'allData-48Vol-High',
-                   'allData-80Vol-Low', 'allData-80Vol-Medium', 'allData-80Vol-High']
-kml_out_files = ['allData-16Vol-Low-drainflow.kml
+#to access json data::::    print data[0]["drainflow"]
 
 with open('doc.kml','rw') as stockKml:
     kmlstr = stockKml.read()
 
 root = parser.fromstring(kmlstr)
-print root.Document.Style.PolyStyle.color.text
 
-for e in root.Document.Folder.Placemark:
-    e.styleUrl = '#transBluePoly'
+json_data_files = ['allData-16Vol-Low.json', 'allData-16Vol-Medium.json', 'allData-16Vol-High.json',
+                   'allData-48Vol-Low.json', 'allData-48Vol-Medium.json', 'allData-48Vol-High.json',
+                   'allData-80Vol-Low.json', 'allData-80Vol-Medium.json', 'allData-80Vol-High.json']
 
-print root.Document.Folder.Placemark.styleUrl
+kml_out_files = ['allData-16Vol-Low-percentCapDrainflow.kml','allData-16Vol-Medium-percentCapDrainflow.kml','allData-16Vol-High-percentCapDrainflow.kml',
+                 'allData-48Vol-Low-percentCapDrainflow.kml','allData-48Vol-Medium-percentCapDrainflow.kml','allData-48Vol-High-percentCapDrainflow.kml',
+                 'allData-16Vol-Low-percentCapDrainflow.kml','allData-48Vol-Medium-percentCapDrainflow.kml','allData-80ol-High-percentCapDrainflow.kml']
 
-new_kml = open('newkml.kml','w')
-new_kml.write(etree.tostring(root,pretty_print=True))
-new_kml.close()
+kml_index = 0
+for datafile in json_data_files:
+   with open(datafile) as data_file:
+     data = json.load(data_file)
+   i = 0
+   while i < 11230 :
+     val = data[i]["percentAnnualCapturedDrainflow"]
+     polygon = root.Document.Folder.Placemark[i]
+     if val < 1 :
+      polygon.styleUrl = '#transBluePoly'
+     elif val < 2 :
+      polygon.styleUrl = '#transRedPoly'
+     i+=1
+   new_kml = open(kml_out_files[kml_index],'w')
+   new_kml.write(etree.tostring(root,pretty_print=True))
+   new_kml.close()
+   kml_index += 1
+
+
+
