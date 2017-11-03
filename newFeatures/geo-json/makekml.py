dfae409d56977d3c6ad413343d9b45628d371a05
Jordan Hagedorn <hagedorj@purdue.edu>
2017-11-02 20:50:16 -0400

    begin filenames for kml generator

--- newFeatures/geo-json/makekml.py
+++ newFeatures/geo-json/makekml.py
@@ -2,6 +2,11 @@ import lxml
 from pykml import parser
 from lxml import etree
 
+json_data_files = ['allData-16Vol-Low', 'allData-16Vol-Medium', 'allData-16Vol-High',
+                   'allData-48Vol-Low', 'allData-48Vol-Medium', 'allData-48Vol-High',
+                   'allData-80Vol-Low', 'allData-80Vol-Medium', 'allData-80Vol-High']
+kml_out_files = ['allData-16Vol-Low-drainflow.kml
+
 with open('doc.kml','rw') as stockKml:
     kmlstr = stockKml.read()
 
